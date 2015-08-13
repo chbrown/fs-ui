115c2a8aa590334d7f24b4d7b5bd267b2aeee5f3
Christopher Brown <io@henrian.com>
2015-08-13 16:13:35 -0500

    Add type declarations

---
+++ type_declarations/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
+/// <reference path="DefinitelyTyped/moment/moment.d.ts" />
+/// <reference path="DefinitelyTyped/node/node.d.ts" />
+/// <reference path="DefinitelyTyped/react/react.d.ts" />
