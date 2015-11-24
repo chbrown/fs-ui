c6104664bf93d997d4662ed0f059c48382c2110b
Christopher Brown <io@henrian.com>
2015-11-24 00:43:22 -0600

    Remove type_declarations/

--- type_declarations/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="DefinitelyTyped/async/async.d.ts" />
-/// <reference path="DefinitelyTyped/lodash/lodash.d.ts" />
-/// <reference path="DefinitelyTyped/moment/moment.d.ts" />
-/// <reference path="DefinitelyTyped/node/node.d.ts" />
-/// <reference path="DefinitelyTyped/react/react.d.ts" />
-
-/// <reference path="../node_modules/notify-ui/notify-ui.d.ts" />
